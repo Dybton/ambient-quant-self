5f031bc9d9d1a82fe143f0851cbfa5e4626616a2
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-02-11 12:08:31 +0100

    fixed repsonsive centering

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="flex h-screen w-screen justify-center items-center" >
-      <div className="w-9/12 h-5/6 mt-6 ">
+      <div className="w-9/12 h-5/6 ">
         <h1 className="text-3xl text-center font-semi-bold">Dashboard</h1>
-        <div className="w-11/12 h-5/6 mt-6 mx-auto"> 
-          <div className="w-5/12 h-full md:float-left ml-14 sm:mr-10">
+        <div className="w-11/12 h-5/6 mt-6 mx-auto mt-10">
+          <div className="w-5/12 h-full lg:float-left border-2 mx-auto lg:ml-14 ">
             <div className="bg-red-500 w-full h-3/6"></div> 
             <div className="bg-blue-500 w-full h-3/6 mt-14"></div>
           </div>
-          <div className="w-5/12 h-full md:float-right mr-14">
+          <div className="w-5/12 h-full lg:float-right border-2 mx-auto lg:mr-14">
             <div className="bg-blue-500 w-full h-3/5 "> 
             </div> 
             <div className="bg-red-500 w-full h-2/5 mt-14">
@@ -31,3 +31,4 @@ export default App;
 // Next up is adding yet another box inside the first box, that is also sized relatively
 // Create 4 boxes inside the second box, that are also sized relatively
 // Create two twin boxes inside the second box, that are also sized relatively
+
