3ca043c69fb45df4f32211253b6e57b215c81db0
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-02-11 12:21:25 +0100

    fixed the responsive vertical distance

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     <div className="flex h-screen w-screen justify-center items-center" >
       <div className="w-9/12 h-5/6 ">
         <h1 className="text-3xl text-center font-semi-bold">Dashboard</h1>
-        <div className="w-11/12 h-5/6 mt-6 mx-auto mt-10">
-          <div className="w-5/12 h-full lg:float-left border-2 mx-auto lg:ml-14 ">
+        <div className="w-11/12 h-5/6 mt-6 mx-auto mt-10 border-2">
+          <div className="w-5/12 h-full lg:float-left border-2 mx-auto lg:ml-14 mb-28 ">
             <div className="bg-red-500 w-full h-3/6"></div> 
             <div className="bg-blue-500 w-full h-3/6 mt-14"></div>
           </div>
@@ -31,4 +31,6 @@ export default App;
 // Next up is adding yet another box inside the first box, that is also sized relatively
 // Create 4 boxes inside the second box, that are also sized relatively
 // Create two twin boxes inside the second box, that are also sized relatively
+// Next up I need to fix the responsive mt
+// I also need to set a respnsive width so that they're always the same
 
