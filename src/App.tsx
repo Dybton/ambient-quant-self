f300517d91c32d8559369e16b9619e1f3bc05223
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-02-11 09:42:52 +0100

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="flex h-screen w-screen justify-center items-center bg-gray-800" >
-      <div className="w-9/12 h-5/6 mt-6 border-2 border-amber-300 justify-center items-center">
+      <div className="w-9/12 h-5/6 mt-6 border-2 border-amber-300">
+        <p className='text-center'>Hej</p>
       </div>
     </div>
 
@@ -14,4 +15,5 @@ function App() {
 
 export default App;
 
-// Ok, so what I want to crate is a div in the center of the page that I can size w and h using relative sizes - done
+// Ok, so what I want to crate is a div in the center of the page that I can size w and h using relative sizes
+// No I need to add a title to the top of the page
