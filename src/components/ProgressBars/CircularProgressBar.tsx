55cfd67e2afe7029a1271546690a9cefc517f138
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-02-16 07:55:29 +0100

    rewritten CircularProgressBar as a ts fc

--- src/components/ProgressBars/CircularProgressBar.tsx
+++ src/components/ProgressBars/CircularProgressBar.tsx
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-// Should probably take percentage as a prop
-function CircularProgressBar() {
-    const percentage = 20; 
-
-  return (
-<div>
-<div className="w-32 mx-auto">
-    <CircularProgressbar value={percentage} text={`${percentage}%`} 
-      styles={{
-        path: { stroke: '#38393E' },
-        text: { fill: '#38393E', fontSize: '18px' },
-      }}
-    />
-  </div>
-</div>
-  );
+type CircularProgressBarProps = {
+  percentage: number;
 }
 
+const CircularProgressBar: React.FC<CircularProgressBarProps> = ({ percentage }) => {
+  return (
+    <div>
+      <div className="w-32 mx-auto">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          styles={{
+            path: { stroke: '#38393E' },
+            text: { fill: '#38393E', fontSize: '14px' },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default CircularProgressBar;
