5bd14639e0c5d35b4abcec244b6c366b6ea9e57f
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-02-18 07:58:36 +0100

    added the rest of the icons

---
+++ src/components/Icons/LinkedinIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Squares: React.FunctionComponent = () => (
+    <svg width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_31_188)">
+        <path d="M23.2644 0.0247498C23.8801 0.0247498 24.4706 0.250465 24.9059 0.652242C25.3413 1.05402 25.5859 1.59895 25.5859 2.16714V17.1639C25.5859 17.7321 25.3413 18.277 24.9059 18.6788C24.4706 19.0806 23.8801 19.3063 23.2644 19.3063H7.01388C6.39818 19.3063 5.8077 19.0806 5.37233 18.6788C4.93697 18.277 4.69238 17.7321 4.69238 17.1639V2.16714C4.69238 1.59895 4.93697 1.05402 5.37233 0.652242C5.8077 0.250465 6.39818 0.0247498 7.01388 0.0247498H23.2644ZM22.684 16.6283V10.951C22.684 10.0248 22.2853 9.13656 21.5757 8.48167C20.866 7.82677 19.9036 7.45885 18.9 7.45885C17.9133 7.45885 16.7642 8.01588 16.207 8.85141V7.66238H12.9685V16.6283H16.207V11.3473C16.207 10.5225 16.9267 9.84762 17.8205 9.84762C18.2515 9.84762 18.6648 10.0056 18.9696 10.2869C19.2743 10.5681 19.4455 10.9496 19.4455 11.3473V16.6283H22.684ZM9.19609 5.9806C9.71328 5.9806 10.2093 5.791 10.575 5.45351C10.9407 5.11602 11.1462 4.65828 11.1462 4.18099C11.1462 3.18478 10.2756 2.37067 9.19609 2.37067C8.67583 2.37067 8.17687 2.5614 7.80899 2.9009C7.4411 3.2404 7.23443 3.70087 7.23443 4.18099C7.23443 5.17721 8.1166 5.9806 9.19609 5.9806ZM10.8095 16.6283V7.66238H7.59426V16.6283H10.8095Z" fill="black"/>
+        </g>
+        <defs>
+        <filter id="filter0_d_31_188" x="0.692383" y="0.0247498" width="28.8936" height="27.2815" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="out"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_31_188"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_31_188" result="shape"/>
+        </filter>
+        </defs>
+    </svg>
+    
+  );
+
+  export default Squares;
---
+++ src/components/Icons/SleepIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SleepIcon: React.FunctionComponent = () => (
+    <svg width="30" height="20" viewBox="0 0 30 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2.22656 2V18M28.2266 13.3333V18M28.2266 13.3333H2.22656M13.9266 4.66667V13.3333H28.2266V6.66667C28.2266 6.13623 28.0211 5.62753 27.6554 5.25245C27.2897 4.87738 26.7937 4.66667 26.2766 4.66667H13.9266Z" stroke="black" stroke-width="2.95849" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M8.07646 9.33334C9.15342 9.33334 10.0265 8.43791 10.0265 7.33334C10.0265 6.22877 9.15342 5.33334 8.07646 5.33334C6.99951 5.33334 6.12646 6.22877 6.12646 7.33334C6.12646 8.43791 6.99951 9.33334 8.07646 9.33334Z" stroke="black" stroke-width="2.95849"/>
+    </svg>
+
+  );
+
+  export default SleepIcon;
