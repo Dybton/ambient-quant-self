6d2cb64ab9a1bfe2440f1beb065e2dbae682e781
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-02-20 07:45:00 +0100

    made the sizing of horizontal progress bar dynamic

--- src/components/ProgressBars/HorizontalProgressBar.tsx
+++ src/components/ProgressBars/HorizontalProgressBar.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 interface HorizontalProgressBarProps {
   height: number;
-  width: number;
+  width: string;
   percentage: number;
-  color: string;
 }
 
 const HorizontalProgressBar: React.FC<HorizontalProgressBarProps> = (props) => {
   return (
-    <div className="w-4/6 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+    <div className={`w-${props.width} bg-gray-200 rounded-full h-${props.height} dark:bg-gray-700`}>
       <div
-        className="bg-blue-600 h-2.5 rounded-full"
+        className={`bg-blue-600 h-${props.height} rounded-full`}
         style={{ width: `${props.percentage}%` }}
       ></div>
     </div>
--- src/components/WebsiteTimeCard.tsx
+++ src/components/WebsiteTimeCard.tsx
@@ -14,38 +14,38 @@ const WebsiteTimeCard: React.FC<Props> = () => {
   return (
     <div className="shadow-lg w-full rounded-3xl h-3/5">
       <div className='flex-row h-1/6 w-full'>
-        <CardHeader title={{ text: "Minutes on Websites", size: "text-xl" }} icon={<WebsitesIcon/>}/>
+        <CardHeader title={{ text: "Minutes on Websites", size: "text-base" }} icon={<WebsitesIcon/>}/>
       </div>
       
-      <div className='flex h-5/6 mt-4 flex-col'>
-        <div className='ml-8 h-1/5 w-full'>
-          <p className='font-bold text-4xl '> 80/160</p>
+      <div className='flex mt-4 flex-col'>
+        <div className='ml-8 mb-1 w-full'>
+          <p className='font-bold text-3xl '> 80/160</p>
         </div>
-        <div className='ml-8 h-1/5 w-full'>
-          <p className='text-1xl '> Facebook</p>
-          <div className='flex flex-row items-center'>
-          <div className='mr-3 w-8'>
-          <FacebookIcon/>
-          </div>
-          <HorizontalProgressBar height={10} width={200} percentage={50} color={'#38393E'}/>
+        <div className='ml-8 h-1/5 w-full mb-3'>
+          <p className='font-light text-sm'> Facebook</p>
+          <div className='flex flex-row items-center mt-1'>
+            <div className='mr-1 w-8'>
+              <FacebookIcon/>
+            </div>
+            <HorizontalProgressBar height={2} width={'4/6'} percentage={20}/>
           </div>
         </div>
-        <div className='ml-8 h-1/5 w-full'>
-          <p> Twitter</p>
-          <div className='flex flex-row items-center'>
-          <div className='mr-3 w-8'>
+        <div className='ml-8 h-1/5 w-full mb-3'>
+          <p className='font-light text-sm'> Twitter</p>
+          <div className='flex flex-row items-center mt-1'>
+          <div className='mr-1 w-8'>
             <TwitterIcon/>
           </div>
-          <HorizontalProgressBar height={10} width={200} percentage={50} color={'#38393E'}/>
+          <HorizontalProgressBar height={2} width={'4/6'} percentage={50}/>
           </div>
         </div>
-        <div className='ml-8 h-1/5 w-full'>
-          <p > Linkedin</p>
-          <div className='flex flex-row items-center'>
-          <div className='mr-3 w-8'>
+        <div className='ml-8 h-1/5 w-full mb-3'>
+          <p className='font-light text-sm'> Linkedin</p>
+          <div className='flex flex-row items-center mt-1'>
+          <div className='mr-1 w-8'>
           <LinkedinIcon/>
           </div>
-          <HorizontalProgressBar height={10} width={200} percentage={50} color={'#38393E'}/>
+          <HorizontalProgressBar height={2} width={'4/6'} percentage={50}/>
           </div>
         </div>
       </div>
