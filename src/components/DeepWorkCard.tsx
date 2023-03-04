c09c18bf6293fd4aadd5b7262bec317354d0f5f1
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-03-04 18:44:38 +0100

    added deepwork card

--- src/components/DeepWorkCard.tsx
+++ src/components/DeepWorkCard.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import CardHeader from './CardHeader';
+import ClockIcon from './Icons/ClockIcon';
+import HorizontalProgressBar from './ProgressBars/HorizontalProgressBar';
 
-interface Props {}
+interface Props {}  
 
 
 const DeepWorkCard: React.FC<Props> = () => {
   return (
-    <div className="shadow-lg w-full rounded-3xl h-2/5 mt-14"></div>
+    <div className="shadow-lg w-full rounded-3xl h-2/5 mt-14">
+      <div className='flex-row h-2/6 w-full'>
+        <CardHeader title={{ text: "Hours of Deep Work", size: "text-base" }} icon={<ClockIcon/>}/>
+      </div>
+      <div className='flex mt-2 flex-col w-full'>
+        <div className='ml-8 mb-2 w-full'>
+          <p className='font-bold text-3xl'> 22.5/30</p>
+        </div>
+        <div className='mt-3' style={{borderTop: '2px solid #E4E2E0', width: '100%'}}></div>
+      </div>
+      <div className='flex flex-col w-full border-2 h-2/5'>
+        <HorizontalProgressBar percentage={75} id={'h4'} h={12} w={'5/6'}/>
+      </div>
+    </div>
   );
 };
+
 
 export default DeepWorkCard;
