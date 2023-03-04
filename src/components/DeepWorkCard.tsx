ae9f24ffde7614e245a4691f2bb1bac24fc1eafd
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-03-04 19:01:58 +0100

    updated DeepWorkCard

--- src/components/DeepWorkCard.tsx
+++ src/components/DeepWorkCard.tsx
@@ -12,14 +12,15 @@ const DeepWorkCard: React.FC<Props> = () => {
       <div className='flex-row h-2/6 w-full'>
         <CardHeader title={{ text: "Hours of Deep Work", size: "text-base" }} icon={<ClockIcon/>}/>
       </div>
-      <div className='flex mt-2 flex-col w-full'>
+      <div className='flex flex-col w-full'>
         <div className='ml-8 mb-2 w-full'>
           <p className='font-bold text-3xl'> 22.5/30</p>
         </div>
-        <div className='mt-3' style={{borderTop: '2px solid #E4E2E0', width: '100%'}}></div>
+        <div className='mt-2' style={{borderTop: '2px solid #E4E2E0', width: '100%'}}></div>
       </div>
-      <div className='flex flex-col w-full border-2 h-2/5'>
+      <div className='ml-8 mt-2 flex flex-col w-full h-2/5 justify-center'>
         <HorizontalProgressBar percentage={75} id={'h4'} h={12} w={'5/6'}/>
+        <p className=' mt-2 font-semibold'>Weekly Goal</p>
       </div>
     </div>
   );
