2ef637f4969f909f3478ba667efda489b856bb6d
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-03-25 00:46:50 +0100

    added new getWeekDayDate function

--- server/src/utilities.ts
+++ server/src/utilities.ts
@@ -30,3 +30,26 @@ export const convertSecondsToTime = (seconds: number) => {
     };
   };
   
+
+  export const getWeekdayDate =(weekday) => {
+    const dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    const inputDayIndex = dayNames.findIndex(day => day.toLowerCase() === weekday.toLowerCase());
+  
+    if (inputDayIndex === -1) {
+      throw new Error("Invalid weekday input. Please enter a valid weekday.");
+    }
+  
+    const today = new Date() ;
+    const currentDayIndex = today.getDay();
+    const adjustedCurrentDayIndex = (currentDayIndex + 6) % 7; // Adjusts the index to treat Monday as the first day
+    const daysUntilInputDay = (inputDayIndex - adjustedCurrentDayIndex + 7) % 7; // Ensures a positive number of days
+  
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(startOfWeek.getDate() - adjustedCurrentDayIndex);
+  
+    const targetDate = new Date(startOfWeek);
+    targetDate.setDate(targetDate.getDate() + inputDayIndex +1);
+  
+    return targetDate;
+  }
+  
