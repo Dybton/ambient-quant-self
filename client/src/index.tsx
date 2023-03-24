76713e63b9d83d314a92b9635f1837fb6d6c8f34
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-03-24 22:04:09 +0100

    updated index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import client from './apolloClient';
+import './index.css';
 import App from './App';
+import client from './apolloClient';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
