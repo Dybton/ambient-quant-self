2605bce2facb33ad1a15c61e236daccd7a65a3c6
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2023-03-28 21:25:30 +0200

    we now differentiate between websites

--- chrome-time-tracker-2/background.js
+++ chrome-time-tracker-2/background.js
@@ -1,56 +1,58 @@
-const targetWebsite = '9gag.com'; // Replace with the domain you want to track
+const targetWebsites = [
+  'facebook.com',
+  'linkedin.com',
+  'youtube.com'
+]; // Replace with the domains you want to track
 let startTime = null;
-let timeSpent = 0;
+let timeSpent = {
+  'facebook.com': 0,
+  'linkedin.com': 0,
+  'youtube.com': 0,
+};
 
-function isTargetWebsite(url) {
-  return url.includes(targetWebsite);
+function getCurrentWebsite(url) {
+  for (const website of targetWebsites) {
+    if (url.includes(website)) {
+      return website;
+    }
+  }
+  return null;
 }
 
-function updateSpentTime() {
-  if (startTime) {
+function updateSpentTime(currentWebsite) {
+  if (startTime && currentWebsite) {
     const now = new Date();
-    timeSpent += (now - startTime) / 1000; // Time spent in seconds
+    timeSpent[currentWebsite] += (now - startTime) / 1000; // Time spent in seconds
     startTime = now;
-    console.log(`Time spent on ${targetWebsite}: ${timeSpent} seconds`);
+    console.log(`Time spent on ${currentWebsite}: ${timeSpent[currentWebsite]} seconds`);
   }
 }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    if (isTargetWebsite(tab.url)) {
-      if (!startTime) {
-        startTime = new Date();
-      }
-    } else {
-      updateSpentTime();
-      startTime = null;
-    }
+    const currentWebsite = getCurrentWebsite(tab.url);
+    updateSpentTime(currentWebsite);
+    startTime = currentWebsite ? new Date() : null;
   }
 });
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
   chrome.tabs.get(activeInfo.tabId, (tab) => {
-    updateSpentTime();
-
-    if (isTargetWebsite(tab.url)) {
-      startTime = new Date();
-    } else {
-      startTime = null;
-    }
+    const currentWebsite = getCurrentWebsite(tab.url);
+    updateSpentTime(currentWebsite);
+    startTime = currentWebsite ? new Date() : null;
   });
 });
 
 chrome.windows.onFocusChanged.addListener((windowId) => {
   if (windowId === chrome.windows.WINDOW_ID_NONE) {
-    updateSpentTime();
+    updateSpentTime(null);
     startTime = null;
   } else {
     chrome.tabs.query({ active: true, windowId: windowId }, (tabs) => {
-      if (tabs[0] && isTargetWebsite(tabs[0].url)) {
-        startTime = new Date();
-      } else {
-        startTime = null;
-      }
+      const currentWebsite = tabs[0] ? getCurrentWebsite(tabs[0].url) : null;
+      updateSpentTime(currentWebsite);
+      startTime = currentWebsite ? new Date() : null;
     });
   }
 });
