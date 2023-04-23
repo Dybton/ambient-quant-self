e17c2ac71b15cb821437a4f7e7d87c6f60febb37
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-04-23 16:16:06 +0200

    updated WebsiteTimeCard to render bars dynamically

--- client/src/components/WebsiteTimeCard.tsx
+++ client/src/components/WebsiteTimeCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CardHeader from './CardHeader';
 import WebsitesIcon from './Icons/WebsitesIcon';
 import HorizontalProgressBar from './ProgressBars/HorizontalProgressBar';
@@ -6,7 +6,6 @@ import FacebookIcon from './Icons/FacebookIcon';
 import TwitterIcon from './Icons/TwitterIcon';
 import LinkedinIcon from './Icons/LinkedinIcon';
 import { gql, useQuery } from '@apollo/client';
-
 
 const WEBSITE_TIME_QUERY = gql`
   query {
@@ -17,12 +16,38 @@ const WEBSITE_TIME_QUERY = gql`
   }
 `;
 
-const WebsiteTimeCard: React.FC = () => {
+enum Website {
+  Facebook = 'facebook.com',
+  Twitter = 'twitter.com',
+  // Linkedin = 'linkedin.com',
+}
 
-  const { loading, error, data } = useQuery(WEBSITE_TIME_QUERY);
+const getIcon = (website: Website) => {
+  switch (website) {
+    case Website.Facebook:
+      return <FacebookIcon />;
+    case Website.Twitter:
+      return <TwitterIcon />;
+    // case Website.Linkedin:
+    //   return <LinkedinIcon />;
+    default:
+      return null;
+  }
+};
+
+const WebsiteTimeCard: React.FC = () => {
+  const { loading, error, data} = useQuery(WEBSITE_TIME_QUERY);
+
+  console.log("data", data)
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+
+  const totalTime = data.timeSpent.reduce((acc: number, curr: { time: number }) => acc + curr.time, 0);
+
+  if (!data.timeSpent || data.timeSpent.length === 0) {
+    return <p>No data currently available.</p>;
+  }
 
   return (
     <div className="shadow-lg w-full rounded-3xl h-3/5">
@@ -32,38 +57,35 @@ const WebsiteTimeCard: React.FC = () => {
       
       <div className='flex mt-4 flex-col'>
         <div className='ml-8 mb-2 w-full'>
-          <p className='font-bold text-3xl '> 80/160</p>
+          <p className='font-bold text-3xl'>{totalTime}/160</p>
         </div>
-        <div className='ml-8 h-1/5 w-full mb-3'>
-          <p className='font-light'> Facebook</p>
-          <div className='flex flex-row items-center mt-1'>
-            <div className='mr-1 w-8'>
-              <FacebookIcon/>
-            </div>
-            <HorizontalProgressBar percentage={20} id={'h1'} h={8} w={'4/6'}/>
-          </div>
-        </div>
-        <div className='ml-8 h-1/5 w-full mb-3'>
-          <p className='font-light '> Twitter</p>
-          <div className='flex flex-row items-center mt-1'>
-          <div className='mr-1 w-8'>
-            <TwitterIcon/>
-          </div>
-          <HorizontalProgressBar percentage={50} id={'h2'} h={8} w={'4/6'}/>
-          </div>
-        </div>
-        <div className='ml-8 h-1/5 w-full mb-3'>
-          <p className='font-light '> Linkedin</p>
-          <div className='flex flex-row items-center mt-1'>
-          <div className='mr-1 w-8'>
-          <LinkedinIcon/>
-          </div>
-          <HorizontalProgressBar percentage={50} id={'h3'} h={8} w={'4/6'}/>
-          </div>
-        </div>
+        {data.timeSpent.map(({ website, time }: { website: string; time: number }) => (
+          <WebSiteSection key={website} website={website as Website} time={time} />
+        ))}
       </div>
     </div>
   );
 };
+
+type WebSiteSectionProps = {
+  website: Website;
+  time: number;
+};
+
+const getWebsitePercentage = (time: number) => {
+  const totalMinutesInDay = 9 * 60;
+  const percentage = (time / totalMinutesInDay) * 100;
+  return percentage;
+}
+
+const WebSiteSection: React.FC<WebSiteSectionProps> = ({ website, time }) => (
+  <div className='ml-8 h-1/5 w-full mb-3'>
+    <p className='font-light'>{website}</p>
+    <div className='flex flex-row items-center mt-1'>
+      <div className='mr-1 w-8'>{getIcon(website)}</div>
+      <HorizontalProgressBar percentage={getWebsitePercentage(time)} id={`h-${website}`} h={8} w={'4/6'} />
+    </div>
+  </div>
+);
 
 export default WebsiteTimeCard;
