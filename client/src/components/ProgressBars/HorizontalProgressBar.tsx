e17692ca48fedf1716a1b04be2fcd4eedadca56d
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-04-23 16:15:13 +0200

    updated the progress bar cards to not exceed 100%

--- client/src/components/ProgressBars/HorizontalProgressBar.tsx
+++ client/src/components/ProgressBars/HorizontalProgressBar.tsx
@@ -9,24 +9,29 @@ interface HorizontalProgressBarProps {
   w?: string;
 }
 
-
 const HorizontalProgressBar: React.FC<HorizontalProgressBarProps> = (props) => {
   return (
     <>
-    <div 
-      className="w-4/6 bg-gray-200 rounded-full dark:bg-gray-700" id={props.id ? `tooltip-id-${props.id}` : ''} 
-      style={{ height: `${props.h}px`}}
-      >
       <div
-        className="bg-blue-600 rounded-full"
-        style={{ width: `${props.percentage}%`, height: `${props.h}px` }}
-      ></div>
-    </div>
-    {props.id && <ReactTooltip
-        anchorId={`tooltip-id-${props.id}`}
-        place="top"
-        content={`${props.percentage}%`}
-      />}
+        className="w-4/6 bg-gray-200 rounded-full dark:bg-gray-700"
+        id={props.id ? `tooltip-id-${props.id}` : ''}
+        style={{ height: `${props.h}px` }}
+      >
+        <div
+          className="bg-blue-600 rounded-full"
+          style={{
+            width: `${Math.min(props.percentage, 100)}%`,
+            height: `${props.h}px`,
+          }}
+        ></div>
+      </div>
+      {props.id && (
+        <ReactTooltip
+          anchorId={`tooltip-id-${props.id}`}
+          place="top"
+          content={`${props.percentage}%`}
+        />
+      )}
     </>
   );
 };
--- client/src/components/ProgressBars/VerticalProgressBar.tsx
+++ client/src/components/ProgressBars/VerticalProgressBar.tsx
@@ -9,24 +9,26 @@ interface VerticalProgressBarProps {
 }
 
 const VerticalProgressBar: React.FC<VerticalProgressBarProps> = (props) => {
-
   return (
     <>
-    <div
-  className="mb-4 h-4/6 bg-gray-200 rounded-full w-2 dark:bg-gray-700 transform rotate-180" id={props.id ? `tooltip-id-${props.id}` : ''} >
       <div
-        className="bg-blue-600 h-full rounded-full"
-        style={{ height: `${props.percentage}%` }}
-      ></div>
-    </div>
-    {props.id && <ReactTooltip
-        anchorId={`tooltip-id-${props.id}`}
-        place="top"
-        content={props.label}
-      />}
+        className="mb-4 h-4/6 bg-gray-200 rounded-full w-2 dark:bg-gray-700 transform rotate-180"
+        id={props.id ? `tooltip-id-${props.id}` : ''}
+      >
+        <div
+          className="bg-blue-600 h-full rounded-full"
+          style={{ height: `${Math.min(props.percentage, 100)}%` }}
+        ></div>
+      </div>
+      {props.id && (
+        <ReactTooltip
+          anchorId={`tooltip-id-${props.id}`}
+          place="top"
+          content={props.label}
+        />
+      )}
     </>
   );
 };
 
 export default VerticalProgressBar;
-
