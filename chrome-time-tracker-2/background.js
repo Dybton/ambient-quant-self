8ea88a633fda40ab138d35920d6c907e5b11778a
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-04-25 20:01:18 +0200

    updated background.js

--- chrome-time-tracker-2/background.js
+++ chrome-time-tracker-2/background.js
@@ -1,31 +1,58 @@
 const targetWebsites = [
   'facebook.com',
   'twitter.com',
-  '9gag.com',
+  'linkedin.com',
 ];
 let activeWebsite = null;
 let timeSpent = {
   'facebook.com': 0,
   'twitter.com': 0,
-  '9gag.com': 0,
+  'linkedin.com': 0,
 };
 
 function getCurrentWebsite(url) {
+  const urlObject = new URL(url);
+  const hostname = urlObject.hostname;
+
   for (const website of targetWebsites) {
-    if (url.includes(website)) {
+    if (hostname.includes(website)) {
       return website;
     }
   }
   return null;
 }
 
+
 function updateSpentTime(currentWebsite) {
   if (currentWebsite) {
+    if (isNaN(timeSpent[currentWebsite])) {
+      timeSpent[currentWebsite] = 0;
+    }
     timeSpent[currentWebsite] += 1; // Increment time spent by 1 second
     console.log(`Time spent on ${currentWebsite}: ${timeSpent[currentWebsite]} seconds`);
     chrome.storage.local.set({ timeSpent }); // Save the updated data to storage
   }
 }
+
+
+function resetTimeSpentIfNeeded() {
+  const today = new Date();
+  const dayOfWeek = today.getDay(); // 0 is Sunday, 1 is Monday, ..., 6 is Saturday
+
+  chrome.storage.local.get(['lastResetDate'], (result) => {
+    const lastResetDate = result.lastResetDate ? new Date(result.lastResetDate) : null;
+
+    if (!lastResetDate || (dayOfWeek === 1 && today.toDateString() !== lastResetDate.toDateString())) {
+      // If there's no lastResetDate, or if today is Monday and not equal to the last reset date, reset the data
+      for (const website in timeSpent) {
+        timeSpent[website] = 0; // Reset the time spent on each website
+      }
+      chrome.storage.local.set({ timeSpent }); // Save the reset data to storage
+      chrome.storage.local.set({ lastResetDate: today.toISOString() }); // Save the new last reset date
+    }
+  });
+}
+
 
 function checkActiveWebsite() {
   chrome.windows.getLastFocused({ populate: true }, (window) => {
@@ -63,6 +90,7 @@ chrome.storage.local.get(['timeSpent'], (result) => {
   if (result.timeSpent) {
     timeSpent = result.timeSpent;
   }
+  resetTimeSpentIfNeeded();
 });
 
 function sendDataToServer(timeSpent) {
@@ -77,7 +105,6 @@ function sendDataToServer(timeSpent) {
     .then((responseText) => console.log(responseText))
     .catch((error) => console.error('Error:', error));
 }
-
 
 // Check the active website and update time spent every second
 setInterval(checkActiveWebsite, 1000);
