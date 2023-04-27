042b85e48bde88b850615056bf937bfac9ba96bf
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-04-27 12:50:44 +0200

    moved website code to websiteService

---
+++ server/src/business_logic/WebsiteService.ts
@@ -0,0 +1,29 @@
+type Context = {
+    timeSpentData: Record<string, number>;
+};
+
+export const fetchTimeSpent = async (context: Context): Promise<{ website: string; time: number }[]> => {
+    try {
+  
+      console.log('fetchTimeSpent context:', context)
+  
+      if (!context || !context.timeSpentData) {
+        console.error('Error: Invalid context or missing timeSpentData');
+        return [];
+      }
+  
+      const result = Object.entries(context.timeSpentData).map(([website, time]) => {
+        if (typeof time !== 'number') {
+          console.warn(`Warning: Time is not a number for ${website}, using 0 as default value`);
+          return { website, time: 0 };
+        }
+        return { website, time };
+      });
+  
+      console.log('fetchTimeSpent result:', result);
+      return result;
+    } catch (error) {
+      console.error('Error in fetchTimeSpent:', error);
+      return [];
+    }
+  };
--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -4,45 +4,15 @@ import { startOfMonth, endOfMonth } from 'date-fns';
 import { fetchSleepData } from './business_logic/SleepService';
 import { fetchDeepWorkHours, updateDeepWorkHours } from './business_logic/DeepWorkService';
 import { fetchRunData } from './business_logic/WorkoutService';
-
-type Context = {
-  timeSpentData: Record<string, number>;
-};
+import { fetchTimeSpent } from './business_logic/WebsiteService';
 
 const { start, end } = getDays();
 const startOfMonthDate = startOfMonth(new Date()).toISOString().split('T')[0];
 const endOfMonthDate = endOfMonth(new Date()).toISOString().split('T')[0];
 
-const fetchTimeSpent = async (context: Context): Promise<{ website: string; time: number }[]> => {
-  try {
-
-    console.log('fetchTimeSpent context:', context)
-
-    if (!context || !context.timeSpentData) {
-      console.error('Error: Invalid context or missing timeSpentData');
-      return [];
-    }
-
-    const result = Object.entries(context.timeSpentData).map(([website, time]) => {
-      if (typeof time !== 'number') {
-        console.warn(`Warning: Time is not a number for ${website}, using 0 as default value`);
-        return { website, time: 0 };
-      }
-      return { website, time };
-    });
-
-    console.log('fetchTimeSpent result:', result);
-    return result;
-  } catch (error) {
-    console.error('Error in fetchTimeSpent:', error);
-    return [];
-  }
-};
-
 export const resolvers = {
   Query: {
     sleepDuration: async () => await fetchSleepData({ start, end }),
-
     runDistance: async () => {
       const [weeklyDistance, monthlyDistance] = await Promise.all([
         fetchRunData({ start, end }),
@@ -61,5 +31,4 @@ export const resolvers = {
   Mutation: {
   updateDeepWorkHours: (_, { date, hours }) => updateDeepWorkHours(_, { date, hours }),
 }
-
-  };
+};
