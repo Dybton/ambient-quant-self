f88e16fe6c14bf2a3461392cff5bca67e53ea93d
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-04-27 12:55:36 +0200

    refactor resolvera bit and added comments

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,34 +1,47 @@
 process.env.TZ = 'Europe/Copenhagen';
-import { getDays} from './utilities';
+import { getWeekStartAndEnd} from './utilities';
 import { startOfMonth, endOfMonth } from 'date-fns';
 import { fetchSleepData } from './business_logic/SleepService';
 import { fetchDeepWorkHours, updateDeepWorkHours } from './business_logic/DeepWorkService';
 import { fetchRunData } from './business_logic/WorkoutService';
 import { fetchTimeSpent } from './business_logic/WebsiteService';
 
-const { start, end } = getDays();
+const { start, end } = getWeekStartAndEnd();
 const startOfMonthDate = startOfMonth(new Date()).toISOString().split('T')[0];
 const endOfMonthDate = endOfMonth(new Date()).toISOString().split('T')[0];
 
 export const resolvers = {
   Query: {
-    sleepDuration: async () => await fetchSleepData({ start, end }),
+    // Fetch sleep data based on the given start and end dates
+    sleepDuration: async () => {
+      return await fetchSleepData({ start, end });
+    },
+
+    // Fetch run data for weekly and monthly distances
     runDistance: async () => {
       const [weeklyDistance, monthlyDistance] = await Promise.all([
         fetchRunData({ start, end }),
         fetchRunData({ start: startOfMonthDate, end: endOfMonthDate }),
-      ])
-      return { weeklyDistance, monthlyDistance};
+      ]);
+      return { weeklyDistance, monthlyDistance };
     },
 
+    // Fetch time spent data from the context
     timeSpent: async (_, __, context) => {
       return await fetchTimeSpent(context);
     },
 
-    deepWorkHours: () =>  fetchDeepWorkHours()
+    // Fetch deep work hours data
+    deepWorkHours: async () => {
+      return await fetchDeepWorkHours();
     },
+  },
 
   Mutation: {
-  updateDeepWorkHours: (_, { date, hours }) => updateDeepWorkHours(_, { date, hours }),
-}
+    // Update deep work hours for a specific date
+    updateDeepWorkHours: async (_, { date, hours }) => {
+      return await updateDeepWorkHours(_, { date, hours });
+    },
+  },
 };
+
