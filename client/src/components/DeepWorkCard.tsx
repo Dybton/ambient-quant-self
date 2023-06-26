4436579f91b83d0b869abc6a146852a2f237d63e
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-06-26 12:55:10 +0200

    added the ability to add deep work hours

--- client/src/components/DeepWorkCard.tsx
+++ client/src/components/DeepWorkCard.tsx
@@ -10,7 +10,7 @@ type DeepWork = {
   date: string;
   deepWorkHours: number;
 };
-  
+
 type DeepWorkHoursData = {
   deepWorkHours: DeepWork[];
 };
@@ -35,13 +35,13 @@ const UPDATE_DEEP_WORK_HOURS = gql`
 
 // Function takes array of Deepwork objects and returns the sum of the deepwork hours
 const sumWeeklyDeepWorkHours = (data: DeepWorkHoursData): number => {
+  const output = data.deepWorkHours.reduce((sum, item) => sum + item.deepWorkHours, 0);
   return data.deepWorkHours.reduce((sum, item) => sum + item.deepWorkHours, 0);
 };
   
 const DeepWorkCard: React.FC = () => {
   const [weeklyDeepWorkHours, setWeeklyDeepWorkHours] = React.useState(0);
 
-  const weeklyDeepworkGoal = 30;
   const { loading, error, data} = useQuery(DEEP_WORK_QUERY);
 
   const [updateDeepWorkHours] = useMutation(UPDATE_DEEP_WORK_HOURS, {
@@ -60,6 +60,7 @@ const DeepWorkCard: React.FC = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  const weeklyDeepworkGoal = 30;
   const calculatePercentage = (hours: number) => {
     return (hours / 30) * 100; //
   };
@@ -73,18 +74,20 @@ const DeepWorkCard: React.FC = () => {
   return (
     <div className="shadow-lg w-full rounded-3xl h-3/5 xl:h-2/5 mt-14 ">
       <div className='flex-row h-2/6 w-full'>
-        <CardHeader title={{ text: "Deep Work", size: 'text-2xl' }} icon={<ClockIcon/>}/>
+        <CardHeader title={{ text: "Hours of Deep Work", size: "text-base" }} icon={<ClockIcon/>}/>
       </div>
       <div className='flex flex-col w-full'>
+        <div className='ml-8 mb-2 w-full'>
+          <p className='font-bold text-3xl'> {weeklyDeepWorkHours}/{weeklyDeepworkGoal}</p>
+        </div>
         <div className='mt-2' style={{borderTop: '2px solid #E4E2E0', width: '100%'}}></div>
       </div>
       <div className='ml-8 mt-2 flex flex-col w-full h-2/5 justify-center'>
-      <p className='font-semibold mr-3'>Weekly Goal</p>
-        <HorizontalProgressBar percentage={calculatePercentage(weeklyDeepWorkHours)} id={'h4'} h={8} w={'5/6'}/>
+        <HorizontalProgressBar percentage={calculatePercentage(weeklyDeepWorkHours)} id={'h4'} h={12} w={'5/6'}/>
         <div className="mt-2">
           <div className="flex flex-row items-center">
-            
-            {/* <PlusIcon onClick={handleIncrement}/> */}
+            <p className='font-semibold mr-3'>Weekly Goal</p>
+            <PlusIcon onClick={handleIncrement}/>
           </div>
         </div>
       </div>
